d9c2a2ca6850ddca730a8bf96b9c5262fb7c1bcd
angelali92 <92.angelali@gmail.com>
2015-08-19 21:52:14 -0700

    qrcode starter code

--- www/js/app.js
+++ www/js/app.js
@@ -52,6 +52,11 @@ angular.module('starter', ['ionic', 'firebase', 'ngCordova'])
             }
          }
       })
+      .state('qrCode', {
+         url: '/qrCode',
+         templateUrl: 'templates/qrcode.html',
+         controller: 'qrController'
+      })
       $urlRouterProvider.otherwise('/tab/list');
 })
 
@@ -110,6 +115,39 @@ angular.module('starter', ['ionic', 'firebase', 'ngCordova'])
   });    
 
 }])
+
+.controller('qrController', function($scope, $http) {
+ 
+    $scope.getData = function() {
+        // $http({
+        //   method: 'GET',
+        //   url: "http://api.qrcode.unitag.fr/api",
+        //   headers: {'Content-Type': 'text/plain'} 
+        // })
+        $http.get("http://api.qrcode.unitag.fr/api", 
+          { 
+            params: 
+              { 
+                "t_pwd": "degraded", 
+                "setting": {},
+                "data":  {
+                  "DATA": {"TEXT": "Hello"},
+                  "TYPE": "text"
+                }
+              } 
+          })
+          .success(function(data) {
+              console.log("success");
+              $scope.image = data;
+              console.log(data);
+              console.log("$scope.image is: " + $scope.image);
+          })
+          .error(function(data) {
+              alert("ERROR");
+          });
+    }
+ 
+});
 
 ;
 
