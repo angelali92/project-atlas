db9a634d47f422ac73f3530e88561fedcebcf0ed
David Nguyen <davidhn1989@gmail.com>
2015-08-23 18:49:20 -0700

    user can now log in

--- www/accounts/login-controller.js
+++ www/accounts/login-controller.js
@@ -1,19 +1,24 @@
 angular.module('linkSpot')
-	.controller('LoginController', ['$scope', function($scope) {
+	.controller('LoginController', ['$scope', '$state', function($scope, $state) {
 
 	    $scope.logIn = function(submittedForm) {
+
+	    	function authHandler(error, authData) {
+	            if (error) {
+	                alert("Login Failed", error);
+	            } else {
+	                alert("Authenticated successfully with payload", authData.uid);
+	                $state.go('tabs.list');
+	            }
+	    	}
 
 	        var ref = new Firebase("https://linkspot.firebaseIO.com/");
 	        ref.authWithPassword({
 	            "email": submittedForm.email,
 	            "password": submittedForm.password  
-	        }, function(error, authData) {
-	            if (error) {
-	                alert("Login Failed", error);
-	            } else {
-	                alert("Authenticated successfully with payload", authData.uid);
-	            }
-	        })
+	        }, authHandler)
 	    }
+
+
 
 	}]);
