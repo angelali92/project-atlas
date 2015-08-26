aabad21a8e60ea9c3457dc02d83595aad401a97e
angelali92 <92.angelali@gmail.com>
2015-08-26 12:54:40 -0700

    linked qrcode and user id together and added to firebase

--- www/accounts/signup-controller.js
+++ www/accounts/signup-controller.js
@@ -15,9 +15,17 @@ angular.module('linkSpot')
 	        			
         				var newUserAuth = ref.getAuth();
 
-	                	ref.child('people').child(newUserAuth.uid).set({
-	                		email: newUserAuth.password.email
-                		});
+            			var url = 'http://api.qrcode.unitag.fr/api?t_pwd=degraded&T=PNG&setting={"LAYOUT":{"COLORBG":"ffffff","GRADIENT_TYPE":"DIAG1","COLOR1":"872BE3", "COLOR2":"DCC7F2"},"EYES":{"EYE_TYPE":"ECurve_ICurve"},"BODY_TYPE":1,"ARRONDI":7}';
+            			url += '&data={"DATA":{"TEXT":"' + newUserAuth.uid + '"},"TYPE":"text"}';
+
+        				fetchQRCode(url, function(imageCode) {
+							ref.child('people').child(newUserAuth.uid).set({
+		                		email: newUserAuth.password.email,
+		                		qrCode: imageCode
+	                		});
+	                		document.getElementById("qr-image").src = imageCode;
+        				});
+	                	
 	        		});
 	               
 	                // $state.go('tabs.list');
@@ -30,5 +38,24 @@ angular.module('linkSpot')
 	            "password": submittedForm.password  
 	        }, authHandler)
 	    }
+
+	    // TAKEN FROM QR CODE CONTROLLER. MAY NEED TO REFACTOR.
+	    var fetchQRCode = function(uri, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', uri, true);
+            xhr.responseType = 'arraybuffer';
+
+            xhr.onload = function(e) {
+                if(this.status == 200) {
+                    var blob = this.response;
+                    var str = String.fromCharCode.apply(null, new Uint8Array(blob));
+                    var imageCode = 'data:image/png;base64,' + btoa(str);
+                    if(callback) {
+                        callback(imageCode);
+                    }
+                }
+            };
+            xhr.send();
+        };
 
 	}]);
